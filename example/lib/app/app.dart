9ef23c0d48a551fb848abdcd912bb1d754c32889
Mattia Pispisa <m.pispisa@mvlabs.it>
2023-04-19 23:45:34 +0200

    remove dep

--- example/lib/app/app.dart
+++ example/lib/app/app.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:example/app/environment.dart';
 import 'package:flutter/material.dart';
 
@@ -21,20 +23,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final Environment environment = Environment.dev;
+  Environment environment = Environment.dev;
 
   @override
   Widget build(BuildContext context) {
     switch (environment) {
       case Environment.dev:
-        
         break;
       default:
     }
     return Scaffold(
       appBar: AppBar(title: Text("Good Enum example")),
       body: Column(children: [
-        Text(environment.isDev() ? "IsDev" : ''),
+        Text(environment.isDev() ? "IsDev" : 'IsNotDev'),
+        environment.map(
+          dev: () => Text("IsDev"),
+          prod: () => Text("IsProd"),
+          stage: () => Text("IsStage"),
+        ),
+        environment.maybeMap(
+          dev: () => Text("IsDev"),
+          orElse: () => Text("IsElse"),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            final random = Random().nextInt(3);
+            final env = Environment.values[random];
+            setState(() {
+              environment = env;
+            });
+          },
+          child: Text("Change env"),
+        )
       ]),
     );
   }
